8049773df468ff9124e848a33df3cf7fafdda4e1
GeorgijSosedov <MarmeladOFForiginal@mail.ru>
2023-06-18 23:02:37 +0500

    rework

--- js/functions.js
+++ js/functions.js
@@ -1,26 +1,31 @@
 const checkLength = (str, num) => str.length <= num;
 
 const checkPalindrome = (str) => {
-    let normalized = str.replaceAll(' ', '').toLowerCase();
-    let reverse = '';
+  const normalized = str.replaceAll(' ', '').toLowerCase();
+  let reverse = '';
+  let counter;
 
-    for(let i = normalized.length - 1; i >= 0; i--) {
-        normalized[i];
-        reverse = reverse += normalized[i];
-    }
+  for(let i = normalized.length - 1; i >= 0; i--) {
+    counter = normalized[i];
+    reverse = reverse += counter;
+  }
 
-    return normalized === reverse ? true : false;
-}
+  return normalized === reverse;
+};
 
 const stringToPositiveNumber = (arg) => {
-    const string = arg.toString();
-    let result = '';
+  const string = arg.toString();
+  let result = '';
 
-    for(let i = 0; i < string.length; i++) {
-        if(!Number.isNaN(parseInt(string[i], 10))) {
-            result += string[i];
-        }
+  for(let i = 0; i < string.length; i++) {
+    if(!Number.isNaN(parseInt(string[i], 10))) {
+      result += string[i];
     }
+  }
 
-    return parseInt(result, 10);
-}
+  return parseInt(result, 10);
+};
+
+checkLength('Какое-то слово');
+checkPalindrome('Лёша на полке клопа нашёл');
+stringToPositiveNumber('коровы 33');
