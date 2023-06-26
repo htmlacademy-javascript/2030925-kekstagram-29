3a197d8214487591322bc8177e05bd45192e08b0
GeorgijSosedov <MarmeladOFForiginal@mail.ru>
2023-06-27 00:26:06 +0500

    rework vol.2

---
+++ js/constants.js
@@ -0,0 +1,22 @@
+
+export const Photos = {
+  MIN_PHOTOS: 1,
+  MAX_PHOTOS: 25,
+  MIN_AVATAR_PHOTOS: 1,
+  MAX_AVATAR_PHOTOS: 6
+};
+
+export const Comments = {
+  MIN_COMMENTS_ID: 1,
+  MAX_COMMENTS_ID: 25,
+  MIN_COMMENTS: 0,
+  MAX_COMMENTS: 30,
+};
+
+export const Posts = {
+  MIN_ID: 1,
+  MAX_ID: 25,
+  MIN_LIKES: 15,
+  MAX_LIKES: 200,
+  MAX_POSTS: 25,
+};
--- js/main.js
+++ js/main.js
@@ -1,24 +1,28 @@
-import { MAX_POSTS_COUNT, RANDOM_AVATAR_URL, RANDOM_COMMENTS_COUNT, RANDOM_DESCRIPTION, RANDOM_LIKES, RANDOM_MESSAGE, RANDOM_PHOTO_URL, RANDOM_USER_ID, RANDOM_NAME, RANDOM_COMMENTS } from './const';
+import {Photos, Posts, Comments } from './constants';
+import {getRandomArrayElement, getRandomIdFromRange, getRandomInteger} from './functions';
+import {mockComments, mockDescriptions, mockMessages, mockNames} from './mocks';
+
 
 const createPost = () =>({
-  id: RANDOM_USER_ID(),
-  url: RANDOM_PHOTO_URL,
-  description: RANDOM_DESCRIPTION,
-  likes: RANDOM_LIKES,
-  comments: RANDOM_COMMENTS
+  id: getRandomIdFromRange(Posts.MIN_ID,Posts.MAX_ID),
+  url:  `photos/${getRandomIdFromRange(1,25)}.jpg`,
+  description: getRandomArrayElement(mockDescriptions),
+  likes: getRandomInteger(Posts.MIN_LIKES, Posts.MAX_LIKES),
+  comments: getRandomArrayElement(mockComments)
 });
 
 const createComment = () =>({
-  id: RANDOM_USER_ID(),
-  avatar: RANDOM_AVATAR_URL,
-  message: RANDOM_MESSAGE,
-  name: RANDOM_NAME
+  id:  getRandomIdFromRange(Comments.MIN_COMMENTS_ID, Comments.MAX_COMMENTS_ID),
+  avatar: `img/avatar-${getRandomInteger(Photos.MIN_AVATAR_PHOTOS, Photos.MAX_AVATAR_PHOTOS)}.svg`,
+  message: getRandomArrayElement(mockMessages),
+  name: getRandomArrayElement(mockNames)
 });
 
 const createMoreComments = () => {
-  Array.from({length: RANDOM_COMMENTS_COUNT}, createComment);
+  Array.from({length:  getRandomInteger(Comments.MIN_COM,Comments.MAX_COM)}, createComment);
 };
-
+createMoreComments();
 const createMorePosts = () => {
-  Array.from({length: MAX_POSTS_COUNT}, createPost);
+  Array.from({length: Posts.MAX_POSTS}, createPost);
 };
+createMorePosts();
