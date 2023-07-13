61b79b5d6334282b43b13aa44204047803f27715
GeorgijSosedov <MarmeladOFForiginal@mail.ru>
2023-07-13 12:17:44 +0500

    Добавил модуль в проект

--- js/create-picture.js
+++ js/create-picture.js
@@ -1,17 +1,20 @@
 import { createMorePosts } from './create-post';
 
-const randomUserPostTemplate = document.querySelector('#picture').content;
 const randomUserPostListElement = document.querySelectorAll('.pictures');
+const randomUserPostTemplate = document.querySelector('#picture').content;
 const randomUserPosts = createMorePosts();
 const randomUserPostFragment = document.createDocumentFragment();
 
-randomUserPosts.forEach((url, description, likes, comments) => {
-  const randomUserPost = randomUserPostTemplate.cloneNode(true);
-  randomUserPost.querySelector('.picture__img').src = url;
-  randomUserPost.querySelector('.picture__img').alt = description;
-  randomUserPost.querySelector('.picture__likes').textContent = likes;
-  randomUserPost.querySelector('.picture__comments').textContent = comments;
-  randomUserPostFragment.appendChild(randomUserPost);
-});
+export const createRandomUserPost = function() {
+    randomUserPosts.forEach((url, description, likes, comments) => {
+        const randomUserPost = randomUserPostTemplate.cloneNode(true);
+        randomUserPost.querySelector('.picture__img').src = url;
+        randomUserPost.querySelector('.picture__img').alt = description;
+        randomUserPost.querySelector('.picture__likes').textContent = likes;
+        randomUserPost.querySelector('.picture__comments').textContent = comments;
+        randomUserPostFragment.appendChild(randomUserPost);
+      });
+      
+      randomUserPostListElement.appendChild(randomUserPostFragment);
+}
 
-randomUserPostListElement.appendChild(randomUserPostFragment);
--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 import { createMorePosts } from './create-post.js';
 import { createMoreComments } from './create-comment.js';
+import { createRandomUserPost } from './create-picture.js';
 
 createMorePosts();
 createMoreComments();
+createRandomUserPost();
