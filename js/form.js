5effea1ad613cf2d48d47627e2a46d41a3e98a0b
GeorgijSosedov <MarmeladOFForiginal@mail.ru>
2023-07-23 01:55:44 +0500

    homework

--- js/form.js
+++ js/form.js
@@ -13,16 +13,14 @@ const isTextFieldFocused = () => document.activeElement === hashtagField || docu
 
 const pristine = new Pristine(uploadForm, {
   classTo: 'img-upload__field-wrapper',
-  errorTextParent: 'img-upload__field-wrapper'
+  errorTextParent: 'img-upload__field-wrapper',
+  errorTextClass: 'img-upload__field-wrapper-error'
 });
 
-const normalizeTags = (tagStr) => {
-  tagStr.trim().split(' ').filter((item) => Boolean(item.length));
-};
+const normalizeTags = (tagStr) => tagStr.trim().split(' ').filter((tag) => Boolean(tag.length));
 
 const checkTagUnique = (tags) => {
   const tagsToLowerCase = normalizeTags(tags).map((tag) => tag.toLowerCase());
-
   return tagsToLowerCase.length === new Set(tagsToLowerCase).size;
 };
 
@@ -32,11 +30,11 @@ const hasValidTags = (tags) => {
   normalizeTags(tags).every((tag) => REGULAR_SYMBOLS.test(tag));
 };
 
-pristine.addValidator(hashtagField, checkTagUnique, ErrorMessage.NON_UNIQUE_HASHTAG, 1 , true);
+pristine.addValidator(hashtagField, checkTagUnique, ErrorMessage.NON_UNIQUE_HASHTAG, 1 , false);
 
-pristine.addValidator(hashtagField, hasValidTags, ErrorMessage.INVALID_HASHTAG_PATTERN, 2 , true);
+pristine.addValidator(hashtagField, hasValidTags, ErrorMessage.INVALID_HASHTAG_PATTERN, 2 , false);
 
-pristine.addValidator(hashtagField, hasValidCount, ErrorMessage.INVALID_HASHTAG_COUNT, 3 , true);
+pristine.addValidator(hashtagField, hasValidCount, ErrorMessage.INVALID_HASHTAG_COUNT, 3 , false);
 
 const onFormSubmit = (evt) => {
   if (!pristine.validate()) {
@@ -63,7 +61,7 @@ function closeOnClick() {
 }
 
 function closeWithEscape(evt) {
-  if (isEscapeKey(evt) && isTextFieldFocused()) {
+  if (isEscapeKey(evt) && !isTextFieldFocused()) {
     evt.preventDefault();
     cancelModal();
   }
