6ce80d0ec0a91d9e2a696e96b029d4fd6970a7e4
GeorgijSosedov <MarmeladOFForiginal@mail.ru>
2023-08-01 21:25:30 +0500

    finally

--- js/scale.js
+++ js/scale.js
@@ -1,7 +1,7 @@
 import { DEFAULT_SCALE, MAX_SCALE, MIN_SCALE, SCALE_STEP } from './constants.js';
 
 const pictureElement = document.querySelector('.img-upload__preview');
-const smallerButtonElement= document.querySelector('.scale__control--smaller');
+const smallerButtonElement = document.querySelector('.scale__control--smaller');
 const biggerButtonElement = document.querySelector('.scale__control--bigger');
 const scaleElement = document.querySelector('.scale__control--value');
 
